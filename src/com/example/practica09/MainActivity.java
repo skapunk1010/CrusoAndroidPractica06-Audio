a111fe61c1bcff1655a493929fe900d63c815218
Erick Martinez <s_e_b_a_s_1010@hotmail.com>
2014-07-10 22:15:12 -0500

    Corregida y con comentarios de las modificaciones y explicacion

--- src/com/example/practica09/MainActivity.java
+++ src/com/example/practica09/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.practica09;
 
+import java.io.File;
+import java.io.IOException;
+
 import android.app.Activity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener{
 	private Button btn01;
@@ -15,6 +20,7 @@ public class MainActivity extends Activity implements OnClickListener{
 	private Button btn03;
 	private Button btnStop;
 	private MediaPlayer musica;
+	private String externalPath;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,28 @@ public class MainActivity extends Activity implements OnClickListener{
 		btn02.setOnClickListener(this);
 		btn03.setOnClickListener(this);
 		btnStop.setOnClickListener(this);
+		
+		//el objeto música no se había instanciado. Ahora se instancia primero como a continuación:
+		musica = new MediaPlayer();
+		
+		/**El método Environment.getExternalStoragePublicDirectory();
+		 * nos regresa el directorio del almacenamiento interno del dispositivo, que sería algo similar a lo siguiente:
+		 * 		storage/sdcard0/
+		 * 
+		 *  El parámetro que recibe el método anterior, que en este caso es Environment.DIRECTORY_MUSIC, nos regresa
+		 *  la carpeta donde como su nombre lo dice, es un directorio donde se almacena música. Android nos da estas
+		 *  constantes ya, y es por eso que aveces nos crea algunas carpetas predeterminadas para audio, video, música
+		 *  y fotos que se realizan con el dispositivo. En este caso, esta constante nos regresa el directorio donde 
+		 *  se almacenan música. ya con este parámetro nos regresa un path diferente. Sería el siguiente:
+		 *  	storage/sdcard0/Music
+		 *  Existen más directorios que se crean con las constantes de la clase Environment, si se desea algún
+		 *  otro las pueden checar en la API de la página de Android
+		 *  
+		 *  En algunos dispositivos el directorio ya está creado. Sino está creado, se debe de crear al menos para este ejemplo.
+		 *  Y dentro del directorio es donde se deben de alojar sus archivos de audio para que se puedan reproducir.
+		 */
+		File file = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC);
+		externalPath = file.getAbsolutePath();
 	}
 
 	@Override
@@ -53,34 +81,68 @@ public class MainActivity extends Activity implements OnClickListener{
 	@Override
 	public void onClick(View view){
 		if(musica != null){
-			musica.release();
+			/** Se cambia el método release() por el reset() por que al llamar al método release(), 
+			 * el estado del objeto música pasaba a terminado, entonces cuando se quería volver a iniciar
+			 * e invocábamos al método prepare(), era un estado inválido y nos marcaba una excepción de tipo
+			 * IlegalStateException. Se puede apreciar mejor en el diagrama de estados que les puse en clase:
+			 * http://developer.android.com/reference/android/media/MediaPlayer.html
+			 */
+			musica.reset();
 		}
-		switch(view.getId())
-		{
-			case R.id.button1: //Clandestino
-				musica = MediaPlayer.create(this, R.raw.clandestino);
-				break;
-			case R.id.button2: //Bongo bong
-				musica = MediaPlayer.create(this, R.raw.bongo_bong);
-				break;
-			case R.id.button3: //Desaparecido
-				musica = MediaPlayer.create(this, R.raw.desaparecido);
-				break;
-			case R.id.button4: //Stop
-				if(musica != null){
-					musica.release();
-				}
-				break;
-			default:
-				break;
-		}
-		if(view.getId() != R.id.button4 && musica != null){
-			musica.seekTo(0);
-			musica.start();
+		System.out.println(externalPath);
+		try {
+			
+			/**Una vez que se hace el switch, se concatena el path con el nombre de la canción.
+			 */
+			switch(view.getId())
+			{
+				case R.id.button1: //Clandestino
+					Toast.makeText(getApplicationContext(), externalPath+"/clandestino.mp3", Toast.LENGTH_SHORT).show();
+					musica.setDataSource(externalPath+"/clandestino.mp3");
+					break;
+				case R.id.button2: //Bongo bong
+					Toast.makeText(getApplicationContext(), externalPath+"/bongo_bong.mp3", Toast.LENGTH_SHORT).show();
+					musica.setDataSource(externalPath+"/bongo_bong.mp3");
+					break;
+				case R.id.button3: //Desaparecido
+					Toast.makeText(getApplicationContext(), externalPath+"/desaparecido.mp3", Toast.LENGTH_SHORT).show();
+					musica.setDataSource(externalPath+"/desaparecido.mp3");
+					break;
+				case R.id.button4: //Stop
+					if(musica != null){
+						musica.reset();
+					}
+					break;
+				default:
+					break;
+			}	
+			if(view.getId() != R.id.button4 && musica != null){
+				/**Aquí se prepara el archivo para reproducirse*/
+				musica.prepare();
+				
+				/**Se posiciona el cursor al principio para la reproducción desde el inicio*/
+				musica.seekTo(0);
+				
+				/**Se inicia la reproducción del archivo de audio*/
+				musica.start();
+			}
+			
+			/**Este bloque de excepciones nos las piden al momento de usar el objeto de la 
+			 * clase MediaPlayer que son los posibles errores en tiempo de ejecución que se pudieran presentar.
+			 * Se podría implementar código según el caso de cada excepción.
+			 */
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
-	
-	
-	
-	
 }
